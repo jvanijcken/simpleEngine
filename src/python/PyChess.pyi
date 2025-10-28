5f9361d6f4b76feb183cd0d5a60f14cbceb63681
jvani <j.vanijcken@gmail.com>
2025-10-28 15:47:10 +0100

    no Board is used, and the exposed funtions now takes all parameters directly

--- src/python/PyChess.pyi
+++ src/python/PyChess.pyi
@@ -2,11 +2,12 @@
 
 from typing import List, Tuple
 
-class Board:
-    def __init__(self, board: List[int], castles: List[int], en_passant: int, color: int) -> None: ...
-    board: List[int]
-    castles: List[int]
-    en_passant: int
-    color: int
 
-def pyIDS(board: Board, color: int, maxDepth: int, durationSeconds: float) -> Tuple[int, List[Board]]: ...
+def time_limited_iterative_deepening_search(
+        pieces: list[int],
+        castle_rights: list[int],
+        en_passant: int,
+        color: int,
+        max_depth: int,
+        duration_seconds: float
+) -> int: ...
